b6af3e4ebdd75593ab614142a508e66828d0e5bd
danny-huasco <daniel_mauricio@live.cl>
2025-04-21 22:24:14 -0400

    first files, uploading to connect with database

---
+++ PedalParadise2/PedalParadise/Models/Client.cs
@@ -0,0 +1,8 @@
+﻿namespace PedalParadise.Models
+{
+    public class Client : User
+    {
+        public int ClientID { get; set; }
+        public string MembershipType { get; set; } // Basic, Premium, etc.
+    }
+}
